1886a63a43ee880b8f020bceb092e97863f1a79c
mongwell <19lion@gmail.com>
2018-04-13 03:13:45 -0500

    command line arguments for cool kids

--- main_linear.cpp
+++ main_linear.cpp
@@ -1,3 +1,4 @@
+#include <stdexcept>
 #include <fstream>
 #include <string>
 #include <random>
@@ -66,30 +67,73 @@ vec3 color(const Ray& r, const Scene& world, unsigned depth = 0) {
 }
 
 int main(int argc, char** argv) {
-    if (argc != 5) {
-        cout << "USAGE: ./render height width samples spheres" << endl;
+    const string ERROR_MSG =
+        "USAGE: render_linear [options] width height [title] samples spheres\n"
+        "Options:\n"
+        "\t-o orthographic rendering\n"
+        "\t-p perspective rendering (default)\n\n"
+        "NOTE: Due to randomized locations/radii and overlapping,"
+        "not all spheres may be visible!";
+
+    vector<string> args(argv + 1, argv + argc + !argc);
+    if (args.size() < 4 || args.size() > 7) {
+        cout << ERROR_MSG << endl;
         return 1;
     }
 
-    int nx = stoi(argv[2]);
-    int ny = stoi(argv[1]);
-    int ns = stoi(argv[3]);
-    unsigned spheres = stoi(argv[4]);
+    vector<string> flags;
+    for (vector<string>::iterator iter = args.end() - 1; iter >= args.begin(); --iter) {
+        if ((*iter)[0] == '-') {
+            flags.push_back(*iter);
+            args.erase(iter);
+        }
+    }
 
-    image<rgb_pixel> prspcImage(nx, ny);
-    //image<rgb_pixel> orthgImage(nx, ny);
-    
+    bool rendPrsp = 0;
+    bool rendOrth = 0;
+    for (string s : flags) {
+        if (s == "-o") rendOrth = 1;
+        else if (s == "-p") rendPrsp = 1;
+        else {
+            cout << ERROR_MSG << endl;
+            return 1;
+        }
+    }
+    rendPrsp = !rendPrsp && !rendOrth ? 1 : rendPrsp;
+
+    string title = "";
+    if (args.size() == 5) {
+        title = args[2] + "-";
+        args.erase(args.begin() + 2);
+    }
 
 
-    PrspcCamera pCam;
-    //OrthgCamera oCam;
+    int nx;
+    int ny;
+    int ns;
+    unsigned spheres;
+    try {
+        nx = stoi(args[0]);
+        ny = stoi(args[1]);
+        ns = stoi(args[2]);
+        spheres = stoi(args[3]);
+    } catch(std::invalid_argument t) {
+        cout << ERROR_MSG << endl;
+        return 1;
+    }
 
-    //vector<Hittable*> scene;
 
     Scene world;
     for (unsigned count = 0; count < spheres; ++count) {
         world.scene.push_back(new Sphere(vec3(random(-2.5, 2.5), random(-1.75, 1.75), random(-5, -2)), random(0.06, 0.1)));
     }
+
+
+    image<rgb_pixel> prspcImage(nx, ny);
+    image<rgb_pixel> orthgImage(nx, ny);
+
+    PrspcCamera pCam;
+    OrthgCamera oCam;
 
     unsigned count = 0;
     for (int j = ny - 1; j >= 0; --j) {
@@ -97,37 +141,50 @@ int main(int argc, char** argv) {
             printProgress(float(count) / (nx * ny));
 
             vec3 pCol(0, 0, 0);
-            //vec3 oCol(0, 0, 0);
+            vec3 oCol(0, 0, 0);
             for (int s = 0; s < ns; s++) {
                 float u = float(i + unitRand()) / float(nx);
                 float v = float(j + unitRand()) / float(ny);
-                Ray pRay = pCam.get_ray(u, v);
-                pCol += color(pRay, world);
 
-                //Ray oRay = oCam.get_ray(u, v);
-                //oCol += color(oRay, world);
+                Ray pRay;
+                if (rendPrsp) {
+                    pRay = pCam.get_ray(u, v);
+                    pCol += color(pRay, world);
+                }
+
+                Ray oRay;
+                if (rendOrth) {
+                    oRay = oCam.get_ray(u, v);
+                    oCol += color(oRay, world);
+                }
+
             }
-            
-            pCol/= float(ns);
-            pCol = sqrt(pCol);
-            int pir = int(255.99*pCol[0]);
-            int pig = int(255.99*pCol[1]);
-            int pib = int(255.99*pCol[2]);
 
-            //oCol/= float(ns);
-            /*oCol = sqrt(oCol);*/
-            //int oir = int(255.99*oCol[0]);
-            //int oig = int(255.99*oCol[1]);
-            /*int oib = int(255.99*oCol[2]);*/
+            if (rendPrsp) {
+                pCol/= float(ns);
+                pCol = sqrt(pCol);
+                int pir = int(255.99*pCol[0]);
+                int pig = int(255.99*pCol[1]);
+                int pib = int(255.99*pCol[2]);
 
-            prspcImage[ny - 1 - j][i] = rgb_pixel(pir, pig, pib);
-            //orthgImage[ny - 1 - j][i] = rgb_pixel(oir, oig, oib);
+                prspcImage[ny - 1 - j][i] = rgb_pixel(pir, pig, pib);
+            }
+
+            if (rendOrth) {
+                oCol/= float(ns);
+                oCol = sqrt(oCol);
+                int oir = int(255.99*oCol[0]);
+                int oig = int(255.99*oCol[1]);
+                int oib = int(255.99*oCol[2]);
+
+                orthgImage[ny - 1 - j][i] = rgb_pixel(oir, oig, oib);
+            }
         }
     }
     printProgress(1);
 
-    prspcImage.write("perspective.png");
-    //orthgImage.write(name + "orthographic.png");
+    if (rendPrsp) prspcImage.write(title + "perspective.png");
+    if (rendOrth) orthgImage.write(title + "orthographic.png");
 
 
     return 0;
