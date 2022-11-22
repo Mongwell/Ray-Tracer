529976ab64e2448cdd264ad4f0f349f5d2c750a9
Maanu Grover <19lion@gmail.com>
2022-11-22 16:10:58 -0600

    write to a jpg file using header-only lib

--- main.cpp
+++ main.cpp
@@ -1,24 +1,37 @@
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image_write.h"
 #include <iostream>
 
 using namespace std;
 
-int main() {
-    const int image_width = 256;
-    const int image_height = 256;
+struct pixel {
+    char r;
+    char g;
+    char b;
+};
+typedef pixel pixel;
 
-    cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+int main() {
+    int image_width = 1920;
+    int image_height = 1080;
+    pixel img_data[image_height][image_width];
 
     for (int i = 0; i < image_height; ++i) {
         for (int j = 0; j < image_width; ++j) {
             double r = double(j) / (image_width - 1);
-            double g = double(i) / (image_height - 1);
+            double g = double(image_height - 1 - i) / (image_height - 1);
             double b = 0.25;
 
-            int r_int = static_cast<int>(255.999 * r);
-            int g_int = static_cast<int>(255.999 * g);
-            int b_int = static_cast<int>(255.999 * b);
+            char r_char = static_cast<int>(255.999 * r);
+            char g_char = static_cast<int>(255.999 * g);
+            char b_char = static_cast<int>(255.999 * b);
 
-            cout << r_int << ' ' << g_int << ' ' << b_int << endl;
+            img_data[i][j].r = r_char;
+            img_data[i][j].g = g_char;
+            img_data[i][j].b = b_char;
         }
     }
+
+    stbi_write_jpg("./main.jpg", image_width, image_height, 3, img_data, 100);
+    return 0;
 }
