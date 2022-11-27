23c30a9332dd19d78147148819f94494386895f4
mongwell <19lion@gmail.com>
2022-11-26 21:08:32 -0600

    add vec3 utility functions

--- src/vec3.cpp
+++ src/vec3.cpp
@@ -1,6 +1,7 @@
 #include "vec3.hpp"
 #include <cmath>
 
+using std::ostream;
 using std::sqrt;
 
 double vec3::operator[](int i) const { return data[i]; }
@@ -29,4 +30,41 @@ double vec3::length_squared() const {
     return data[0] * data[0] + data[1] * data[1] + data[2] * data[2];
 }
 
+double vec3::sum() const { return data[0] + data[1] + data[2]; }
+
 double vec3::length() const { return sqrt(length_squared()); }
+
+// Utility Functions Definitions //
+
+ostream &operator<<(ostream &out, const vec3 &v) {
+    return out << v[0] << ", " << v[1] << ", " << v[2];
+}
+
+vec3 operator+(const vec3 &u, const vec3 &v) {
+    return vec3(u[0] + v[0], u[1] + v[1], u[2] + v[2]);
+}
+
+vec3 operator-(const vec3 &u, const vec3 &v) {
+    return vec3(u[0] - v[0], u[1] - v[1], u[2] - v[2]);
+}
+
+vec3 operator*(const vec3 &u, const vec3 &v) {
+    return vec3(u[0] * v[0], u[1] * v[1], u[2] * v[2]);
+}
+
+vec3 operator*(const double t, const vec3 &v) {
+    return vec3(t * v[0], t * v[1], t * v[2]);
+}
+
+vec3 operator*(const vec3 &v, const double t) { return t * v; }
+
+vec3 operator/(const vec3 &v, double t) { return (1 / t) * v; }
+
+double dot(const vec3 &u, const vec3 &v) { return (u * v).sum(); }
+
+vec3 cross(const vec3 &u, const vec3 &v) {
+    return vec3(u[1] * v[2] - u[2] * v[1], u[2] * v[0] - u[0] * v[2],
+                u[0] * v[1] - u[1] * v[0]);
+}
+
+vec3 unit_vector(const vec3 &v) { return v / v.length(); }
--- src/vec3.hpp
+++ src/vec3.hpp
@@ -1,6 +1,8 @@
 #ifndef VEC3_H
 #define VEC3_H
 
+#include <ostream>
+
 class vec3 {
   private:
     double data[3];
@@ -31,6 +33,7 @@ class vec3 {
     vec3 &operator*=(const double m);
     vec3 &operator/=(const double d);
 
+    double sum() const;
     double length_squared() const;
     double length() const;
 };
@@ -38,5 +41,17 @@ class vec3 {
 // Type aliases
 using point3 = vec3;
 using color = vec3;
+
+// Utility Functions //
+std::ostream &operator<<(std::ostream &out, const vec3 &v);
+vec3 operator+(const vec3 &u, const vec3 &v);
+vec3 operator-(const vec3 &u, const vec3 &v);
+vec3 operator*(const vec3 &u, const vec3 &v);
+vec3 operator*(const double t, const vec3 &v);
+vec3 operator*(const vec3 &v, const double t);
+vec3 operator/(const vec3 &v, double t);
+double dot(const vec3 &u, const vec3 &v);
+vec3 cross(const vec3 &u, const vec3 &v);
+vec3 unit_vector(const vec3 &v);
 
 #endif
