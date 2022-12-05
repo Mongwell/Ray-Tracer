9a4decf69f0df16fa7321b08543f0b7e841472b2
mongwell <19lion@gmail.com>
2022-12-04 19:15:08 -0600

    set the scene

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "ray.hpp"
 #include "stb_image_write.h"
 #include "vec3.hpp"
 #include <iostream>
@@ -26,17 +27,41 @@ void print_progress_meter(const float progress) {
     cerr << '\r' << meter << static_cast<int>(progress * 100) << '%' << flush;
 }
 
+color ray_color(const ray &r) {
+    const color bg_color1(0.5, 0.7, 1.0);
+    const color bg_color2(1, 1, 1);
+    vec3 unit_direction = unit_vector(r.direction());
+    double t = 0.5 * (unit_direction.y() + 1.0);
+    return (1.0 - t) * bg_color2 + t * bg_color1;
+}
+
 int main() {
+    // Image settings
+    const double aspect_ratio = 16.0 / 9.0;
     const int image_width = 1920;
-    const int image_height = 1080;
+    const int image_height = static_cast<int>(image_width / aspect_ratio);
+
+    // Camera settings
+    double viewport_height = 2.0;
+    double viewport_width = aspect_ratio * viewport_height;
+    double focal_length = 1.0;
+    point3 origin(0, 0, 0);
+    vec3 horizontal(viewport_width, 0, 0);
+    vec3 vertical(0, viewport_height, 0);
+    vec3 depth(0, 0, focal_length);
+    point3 upper_left_corner = origin - horizontal / 2 + vertical / 2 - depth;
+
     pixelColor img_data[image_height][image_width];
 
     for (int i = 0; i < image_height; ++i) {
         print_progress_meter((i + 1) / static_cast<float>(image_height));
         for (int j = 0; j < image_width; ++j) {
-            color pixel(double(j) / (image_width - 1),
-                        double(image_height - 1 - i) / (image_height - 1),
-                        0.25);
+            double u = double(j) / (image_width - 1);
+            double v = double(i) / (image_height - 1);
+            point3 target = upper_left_corner + u * horizontal - v * vertical;
+            ray r(origin, target - origin);
+
+            color pixel = ray_color(r);
             img_data[i][j] = pixel.to_pixel_color();
         }
     }
